b49db5a656c72f1a5bd418b9a5a30e88fcdf9c77
ben <ben@dreamerslab.com>
2012-12-05 13:18:17 +0800

    add routes

--- config/routes.js
+++ config/routes.js
@@ -1,3 +1,22 @@
 module.exports = function ( map ){
-  map.get( '/','welcome#index' );
+  map.get( '/','home#index' );
+
+  map.resources( 'news',     { only : [ 'index', 'show' ]});
+  map.resources( 'artists',  { only : [ 'index', 'show' ]});
+  map.resources( 'releases', { only : [ 'index', 'show' ]});
+
+  map.get( 'videos',  'videos#index' );
+  map.get( 'live',    'lives#index' );
+  map.get( 'contact', 'contact#index' );
+
+  map.get( 'admin', 'admin/home#index' );
+  map.namespace( 'admin', function ( admin ){
+    admin.get( 'edit',  'home#edit' );
+    map.put(   'admin', 'home#update' );
+    admin.resources( 'news' );
+    admin.resources( 'artists' );
+    admin.resources( 'releases' );
+    admin.resources( 'videos', { except : [ 'show' ]});
+    admin.resources( 'live',   { except : [ 'show' ]});
+  });
 };
