da5bd8d5cf2996777abfa60a91a99dc1c6002757
Fred Chu <cjw3112386@gmail.com>
2013-01-03 17:42:32 +0800

    minor style bug fixed, added admin videos index view and style

--- app/controllers/admin/videos.js
+++ app/controllers/admin/videos.js
@@ -11,7 +11,10 @@ module.exports = Application.extend({
   },
 
   index : function ( req, res, next ){
-    res.render( 'admin/videos/index' );
+    res.render( 'videos/index', {
+      _assets          : 'admin/videos/assets/_index',
+      is_authenticated : req.session.is_authenticated
+    });
   },
 
   edit : function ( req, res, next ){
--- app/controllers/videos.js
+++ app/controllers/videos.js
@@ -3,6 +3,8 @@ var Application = require( CONTROLLER_DIR + 'application' );
 module.exports = Application.extend({
 
   index : function ( req, res, next ){
-    res.render( 'videos/index' );
+    res.render( 'videos/index', {
+      _assets : 'videos/assets/_index'
+    });
   }
 });
