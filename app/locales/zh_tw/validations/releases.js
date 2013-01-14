cc694462bfc4d8558380975cfbc807c501420c86
Fred Chu <cjw3112386@gmail.com>
2013-01-14 17:15:30 +0800

    added validations about releases

---
+++ app/locales/zh_tw/validations/releases.js
@@ -0,0 +1,13 @@
+module.exports = {
+  _404 : '404',
+
+  invalid : {
+    name : '請將名字縮短到 25 字以內',
+    desc : '請將介紹縮短到 500 字以內'
+  },
+
+  required : {
+    name : '名字為必填',
+    desc : '介紹為必填'
+  }
+};
