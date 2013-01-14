7f01b6feeea0c624eca0d7985e1c7e9476ee4b4f
Fred Chu <cjw3112386@gmail.com>
2013-01-14 17:25:23 +0800

    added validation rule to validation about releases

--- app/locales/zh_tw/validations/releases.js
+++ app/locales/zh_tw/validations/releases.js
@@ -2,8 +2,9 @@ module.exports = {
   _404 : '404',
 
   invalid : {
-    name : '請將名字縮短到 25 字以內',
-    desc : '請將介紹縮短到 500 字以內'
+    name         : '請將名字縮短到 25 字以內',
+    desc         : '請將介紹縮短到 500 字以內',
+    release_date : '請將發表日期縮短到 10 個字母以內'
   },
 
   required : {
