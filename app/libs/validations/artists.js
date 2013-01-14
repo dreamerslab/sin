b99d41ed180b5ad1f6cbb40a22d761c1289c5ebf
Fred Chu <cjw3112386@gmail.com>
2013-01-14 16:49:34 +0800

    added validations about artists

---
+++ app/libs/validations/artists.js
@@ -0,0 +1,20 @@
+var form  = require( 'express-form2' );
+var field = form.field;
+var lang  = require( LANG_DIR + 'en_us/validations' );
+var r     = require( './regex' );
+
+form.configure({
+  autoTrim : true
+});
+
+module.exports = {
+
+  validate_show : form(
+    field( 'id' ).is( r.id, lang._404 )
+  ),
+
+  validate_create_n_update : form(
+    field( 'name' ).required( '', lang.required.name ).maxLength( 50, lang.invalid.name ),
+    field( 'desc' ).required( '', lang.required.desc ).maxLength( 1000, lang.invalid.desc )
+  )
+};
---
+++ app/locales/zh_tw/validations/artists.js
@@ -0,0 +1,19 @@
+module.exports = {
+ _404 : '404',
+
+ invalid : {
+    // sample code
+    // username_length : 'Username is too short or too long.',
+    // password_length : 'Password is too short or too long.',
+    // email           : 'Not a valid email address.',
+    // password_match  : 'These passwords don\'t match.',
+    // key             : 'Invalid key.'
+    name : '請將名字縮短到 25 字以內',
+    desc : '請將介紹縮短到 500 字以內'
+  },
+
+ required : {
+  name : '名字為必填',
+  desc : '介紹為必填'
+ }
+};
