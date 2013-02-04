3a94ef4b8418f6d17b5569bc4e46cfad7cd3e577
Fred Chu <cjw3112386@gmail.com>
2013-02-04 15:42:07 +0800

    added content to contact controller, modified mailer lib

--- app/controllers/contact.js
+++ app/controllers/contact.js
@@ -1,5 +1,6 @@
 var Application = require( CONTROLLER_DIR + 'application' );
 var validations = require( LIB_DIR + 'validations/contact' );
+var mailer      = require( LIB_DIR + 'mailer' );
 
 module.exports = Application.extend( validations, {
 
@@ -15,89 +16,20 @@ module.exports = Application.extend( validations, {
   },
 
   create : function ( req, res, next ){
-    // send email, render contact index view with success msg
-    res.render( 'contact/index' );
+    var args = req.form;
+
+    if( !req.form.isValid ){
+      return res.render( 'contact/index', {
+        ori_body : req.body
+      });
+    }
+
+    mailer( args, function ( err, msg ){
+      if( err ) return next( err );
+
+      res.render( 'contact/index', {
+        is_sent : true
+      });
+    });
   }
 });
-
-
-
-
-// copied from cvl-coke, use this later
-
-// var Controller = require( CONTROLLER_DIR + 'application' );
-// var validate   = require( LIB_DIR + 'validations/contact' );
-// var mailer     = require( LIB_DIR + 'mailer' );
-// var lang       = require( LANG_DIR + 'en_us/contact' );
-
-// module.exports = Controller.extend( validate, {
-
-//   init : function ( before, after ){
-//     before( this.locals );
-//     before( this.validate, { only : [ 'send' ]});
-//     before( this.is_valid, { only : [ 'send' ]});
-//   },
-
-// //--- before filters -----------------------------------------------------------
-
-//   locals : function ( req, res, next ){
-//     var keywords = [
-//       'Coverline',
-//       'contact'
-//     ].join( ', ' );
-
-//     res.locals({
-//       title       : lang.title,
-//       heading     : lang.heading.default,
-//       url         : req.headers.referer || '/',
-//       text        : 'Go Back',
-//       keywords    : keywords,
-//       description : lang.heading.default
-//     });
-
-//     next();
-//   },
-
-//   is_valid : function ( req, res, next ){
-//     if( req.form.isValid ) return next();
-
-//     var errs = req.form.getErrors;
-
-//     res.render( 'contact/index', {
-//       user : req.body,
-//       url  : req.body.url,
-//       err  : {
-//         email : errs( 'email' )[ 0 ],
-//         name  : errs( 'name' )[ 0 ],
-//         msg   : errs( 'msg' )[ 0 ]
-//       }
-//     });
-//   },
-
-// //--- actions ------------------------------------------------------------------
-
-//   index : function ( req, res, next ){
-//     res.render( 'contact/index' );
-//   },
-
-//   send : function ( req, res, next ){
-//     var self   = this;
-//     var locals = {};
-
-//     mailer( req.form, function ( err, msg ){
-//       if( err ){
-//         locals.err = {
-//           msg : lang.error
-//         };
-
-//         LOG.error( 500, res, err );
-//       }else{
-//         locals.heading = lang.heading.sent;
-//       }
-
-//       locals.url = req.body.url;
-
-//       res.render( 'contact/index', locals );
-//     });
-//   }
-// });
--- app/libs/mailer.js
+++ app/libs/mailer.js
@@ -4,9 +4,9 @@ var server = email.server.connect( CONF.mail );
 module.exports = function ( args, callback ){
   server.send({
     from    :  args.email,
-    // to      :  'fred <cjw3112386@gmail.com>', // for test
-    to      : 'ramon@coverline.com.tw',
-    subject : '[coverline.com.tw]Comments',
-    text    :  args.msg  + ' Message from ' + args.name
+    to      :  'fred <cjw3112386@gmail.com>', // for test
+    // to      : 'ramon@coverline.com.tw',
+    subject : '[Sincerely Music][Comments]' + args.subject,
+    text    :  args.msg
   }, callback );
 };
