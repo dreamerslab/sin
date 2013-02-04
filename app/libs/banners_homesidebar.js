5e120cc32f8e2493f9c8b8d2a43b7d7fbd557d72
Fred Chu <cjw3112386@gmail.com>
2013-02-04 15:43:37 +0800

    added lib for creating banners, home_soundcloud and home_facebook

---
+++ app/libs/banners_homesidebar.js
@@ -0,0 +1,33 @@
+var Url = Model( 'Url' );
+
+var urls = [
+  'home',
+  'home_soundcloud',
+  'home_facebook',
+  'news',
+  'artists',
+  'releases',
+  'live',
+  'videos',
+  'contact'
+];
+
+var msg_pf = '[libs][urls] ';
+
+var error = function ( err ){
+  LOG.error( 500, msg_pf + 'Fail to create url', err );
+};
+
+var success = function ( url ){
+  LOG.debug( msg_pf + 'Successfully created url', url );
+};
+
+module.exports = {
+  init : function (){
+    urls.forEach( function ( type ){
+      Url.insert({
+        type : type
+      }, error, success );
+    });
+  }
+};
