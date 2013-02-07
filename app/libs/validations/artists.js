8004d5fea5def56f9c8a2c81d39b8930213d85bb
Fred Chu <cjw3112386@gmail.com>
2013-02-07 18:22:47 +0800

    added validation rule and error msg for artists create and update

--- app/libs/validations/artists.js
+++ app/libs/validations/artists.js
@@ -18,8 +18,13 @@ module.exports = {
   ),
 
   validate_create_n_update : form(
-    field( 'id'   ).is( r.id, lang._404 ),
-    field( 'name' ).required( '', lang.required.name ).maxLength( 50,   lang.invalid.name ),
+    field( 'id'    ).is( r.id, lang._404 ),
+    field( 'link1' ).isUrl( lang.invalid.link1_not_url ).is( r.has_http_or_https, lang.invalid.link1_no_http_or_https ),
+    field( 'link2' ).isUrl( lang.invalid.link2_not_url ).is( r.has_http_or_https, lang.invalid.link2_no_http_or_https ),
+    field( 'link3' ).isUrl( lang.invalid.link3_not_url ).is( r.has_http_or_https, lang.invalid.link3_no_http_or_https ),
+    field( 'link4' ).isUrl( lang.invalid.link4_not_url ).is( r.has_http_or_https, lang.invalid.link4_no_http_or_https ),
+    field( 'link5' ).isUrl( lang.invalid.link5_not_url ).is( r.has_http_or_https, lang.invalid.link5_no_http_or_https ),
+    field( 'name' ).required( '', lang.required.name ).maxLength( 50, lang.invalid.name ),
     field( 'desc' ).maxLength( 1000, lang.invalid.desc )
   )
 };
--- app/locales/zh_tw/validations/artists.js
+++ app/locales/zh_tw/validations/artists.js
@@ -2,8 +2,18 @@ module.exports = {
   _404 : '404',
 
   invalid : {
-    name : '請將名字縮短到 25 字以內',
-    desc : '請將介紹縮短到 500 字以內'
+    name                   : '請將名字縮短到 25 字以內',
+    desc                   : '請將介紹縮短到 500 字以內',
+    link1_not_url          : '請在第一個連結輸入符合格式的網址',
+    link2_not_url          : '請在第二個連結輸入符合格式的網址',
+    link3_not_url          : '請在第三個連結輸入符合格式的網址',
+    link4_not_url          : '請在第四個連結輸入符合格式的網址',
+    link5_not_url          : '請在第五個連結輸入符合格式的網址',
+    link1_no_http_or_https : '請在第一個連結加上 http:// 或是 https://',
+    link2_no_http_or_https : '請在第二個連結加上 http:// 或是 https://',
+    link3_no_http_or_https : '請在第三個連結加上 http:// 或是 https://',
+    link4_no_http_or_https : '請在第四個連結加上 http:// 或是 https://',
+    link5_no_http_or_https : '請在第五個連結加上 http:// 或是 https://'
   },
 
   required : {
