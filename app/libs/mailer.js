b14ecd57353f171f024997318302bf93689a886d
Fred Chu <cjw3112386@gmail.com>
2013-02-07 19:42:11 +0800

    removed useless code in mailer.js

--- app/libs/mailer.js
+++ app/libs/mailer.js
@@ -5,7 +5,6 @@ module.exports = function ( args, callback ){
   server.send({
     from    :  args.email,
     to      :  'fred <fred@dreamerslab.com>', // for test
-    // to      : 'ramon@coverline.com.tw',
     subject : '[Sincerely Music][Comments]' + args.subject,
     text    :  args.msg
   }, callback );
