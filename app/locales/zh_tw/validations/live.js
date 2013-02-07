93aa756d06ec26f597c0c6f9f0d890fd019c19f9
Fred Chu <cjw3112386@gmail.com>
2013-02-07 18:44:08 +0800

    added validation rule and error msg for live create and update

--- app/locales/zh_tw/validations/live.js
+++ app/locales/zh_tw/validations/live.js
@@ -1,9 +1,10 @@
 module.exports = {
   invalid : {
-    title    : '請將標題縮短到 25 字以內',
-    date     : '請將舉辦日期縮短到 500 字以內',
-    location : '請將舉辦地點縮短到 25 字以內',
-    url      : '請輸入符合規格的網址'
+    title            : '請將標題縮短到 25 字以內',
+    date             : '請將舉辦日期縮短到 500 字以內',
+    location         : '請將舉辦地點縮短到 25 字以內',
+    url              : '請輸入符合規格的網址',
+    no_http_or_https : '請在網址前面加上 http:// 或是 https://'
   },
 
   required : {
