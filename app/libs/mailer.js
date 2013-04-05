33e07c349331c1edf4624fdd80689f244f90ca2f
ben <ben@dreamerslab.com>
2013-04-05 21:43:29 +0800

    update mail receiver

--- app/libs/mailer.js
+++ app/libs/mailer.js
@@ -3,9 +3,9 @@ var server = email.server.connect( CONF.mail );
 
 module.exports = function ( args, callback ){
   server.send({
-    from    :  args.email,
-    to      :  'Sincerely Music <musicsincerely777@gmail.com >',
+    from    : args.email,
+    to      : 'Sincerely Music <info@sincerely30.com>',
     subject : '[Sincerely Music][Comments]' + args.subject,
-    text    :  args.msg
+    text    : args.msg
   }, callback );
 };
